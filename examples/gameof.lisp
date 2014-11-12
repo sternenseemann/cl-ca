b24e3fa47ad09a572f06511ff1ed11088b5bcd7c
Lukas Epple <git@lukasepple.de>
2014-11-12 14:48:18 +0100

    cleaned up gameof.lisp also now using cl-ca:print-automata

--- examples/gameof.lisp
+++ examples/gameof.lisp
@@ -1,55 +1,52 @@
+;;; gameof.lisp
+;;; example program implementing Conway's game of life
+
 (ql:quickload :cl-ca)
 
 (setf *random-state* (make-random-state t)) ; we want more random stuff /o/
-(defparameter *width* 80 "width of the simulation")
-(defparameter *height* 24 "height of the simulation")
+(defparameter *from* '(0 . 0) "starting point of the simulation")
+(defparameter *to* '(80 . 24) "ending point of the simulation")
 
 (defun liferules (automaton neighbors)
   "implements the rules of Conway's Game of Life for cl-ca"
   (let ((alive-neighbors (reduce (lambda (val el) 
-				   (+ val (car el))) 
-				 neighbors :initial-value 0)))
-    (cond
-      ((and (= (car automaton) 1) (> alive-neighbors 3))                          (list 0 (cadr automaton)))
-      ((and (= (car automaton) 1) (< alive-neighbors 2))                          (list 0 (cadr automaton)))
-      ((and (= (car automaton) 1) (>= alive-neighbors 2) (<= alive-neighbors 3))  (list 1 (cadr automaton)))
-      ((and (= (car automaton) 0) (= alive-neighbors 3))                          (list 1 (cadr automaton)))
-      ((and (= (car automaton) 0) (not (= alive-neighbors 3)))                    (list 0 (cadr automaton)))
-      )))
-
-(defun printgame ()
-  "prints the simulation"
-  #+clisp
-  (ext:shell "clear")
-  #+sbcl
-  (sb-ext:run-program "/bin/sh" (list "-c" "clear") :input nil :output *standard-output*)
-  (loop for y
-	upto *height*
-	do (progn 
-	     (loop for x
-		 upto *width*
-		 do (format t "~a" (if (= 1 (car (cl-ca:get-automaton (cons x y))))
-				     #\#
-				     #\space)))
-	     (format t "~%"))))
+                                   (if (car el)
+                                     (1+ val)
+                                     val))
+                                 neighbors :initial-value 0)))
+    (list 
+      ; the cond construct determinates the new state
+      (cond
+        ((and (car automaton)       (> alive-neighbors 3))                         'nil)
+        ((and (car automaton)       (< alive-neighbors 2))                         'nil)
+        ((and (car automaton)       (>= alive-neighbors 2) (<= alive-neighbors 3)) 't)
+        ((and (not (car automaton)) (= alive-neighbors 3))                         't)
+        ((and (not (car automaton)) (not (= alive-neighbors 3)))                   'nil)
+        )
+      (cadr automaton))))
 
 (defun init-random (pos)
   "returns the initial state of each automaton"
-  (random 2))
+  (if (= 0 (random 2))
+    'nil
+    't))
 
 (defun only-glider (pos)
   "inits the simulation so that there's only one hacker glider :)"
   (cond
-    ((equal pos '(1 . 0)) 1)
-    ((equal pos '(2 . 1)) 1)
-    ((equal pos '(2 . 2)) 1)
-    ((equal pos '(0 . 2)) 1)
-    ((equal pos '(1 . 2)) 1)
-    (t 0)))
+    ((equal pos '(1 . 0)) 't)
+    ((equal pos '(2 . 1)) 't)
+    ((equal pos '(2 . 2)) 't)
+    ((equal pos '(0 . 2)) 't)
+    ((equal pos '(1 . 2)) 't)
+    (t 'nil)))
 
-(cl-ca:create-automata (cons 0 0) (cons *width* *height*) #'init-random #'liferules)
+(cl-ca:create-automata *from* *to* #'init-random #'liferules)
 
 (loop do (progn
-	   (printgame)
+	   (cl-ca:print-automata *from* *to* (lambda (x)
+                                           (if x
+                                             #\#
+                                             #\space)))
 	   (sleep 1)
-	   (cl-ca:run-step (cons 0 0) (cons *width* *height*) #'cl-ca:moore-neighbors)))
+	   (cl-ca:run-step *from* *to* #'cl-ca:moore-neighbors)))
